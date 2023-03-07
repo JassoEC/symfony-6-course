0824d27ccf94394ed4e218bb6f6c180c390b9175
Emanuel Campos <isw.ecamposj@gmail.com>
2023-03-06 22:30:35 -0600

    cache service

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,40 +3,49 @@
 namespace App\Controller;
 
 use DateTime;
+use Psr\Cache\CacheItemInterface;
+
+use function Symfony\Component\String\u;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class VinylController extends AbstractController
 {
   #[Route('/', name: 'app_homepage')]
   public function homepage()
   {
-    $mixes = $this->getMixes();
+    $mixes = [
+      ['song' => 'Ojos negros', 'artist' => 'El Gran Combo de Puerto Rico', 'createdAt' => new DateTime('2021-10-02')],
+      ['song' => 'Quimbara', 'artist' => 'Fania', 'createdAt' => new DateTime('2020-10-02')],
+    ];
 
     return $this->render('vinyl/homepage.html.twig', [
       'mixes' => $mixes,
-      'title' => 'Trucutu'
+      'title' => 'Trucutu',
     ]);
-
   }
 
   #[Route('/browse/{slug}', name: 'app_browse')]
-  public function browse( string $slug = null)
-  {
-    $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-    $mixes = $this->getMixes();
-    
+  public function browse(
+    HttpClientInterface $httpClient,
+    CacheInterface $cache,
+    string $slug = null
+  ) {
+
+    $genre    = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+    $mixes = $cache->get('mixes', function (CacheItemInterface $cacheItem) use ($httpClient) {
+      $cacheItem->expiresAfter(5);
+      $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+      return $response->toArray();
+    });
+
+
     return $this->render('vinyl/browse.html.twig', [
       'genre' => $genre,
-      'mixes' => $mixes
+      'mixes' => $mixes,
     ]);
-  }
-
-  private function getMixes(): array{
-    return [
-      ['song' => 'Ojos negros', 'artist' => 'El Gran Combo de Puerto Rico','createdAt'=> new DateTime('2021-10-02')],
-      ['song' => 'Quimbara', 'artist' => 'Fania','createdAt'=> new DateTime('2020-10-02')],
-    ];
   }
 }
